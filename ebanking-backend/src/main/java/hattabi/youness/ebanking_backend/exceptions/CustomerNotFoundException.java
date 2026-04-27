3e115920876c54d2b872bb6b1b7c1545767fbb20
abdoheddaya <heddayaabderrahmane@gmail.com>
2026-04-27 11:47:15 +0100

    Added exceptions.

---
+++ ebanking-backend/src/main/java/hattabi/youness/ebanking_backend/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package hattabi.youness.ebanking_backend.exceptions;
+
+public class CustomerNotFoundException extends Exception {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
