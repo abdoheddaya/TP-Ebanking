19a4187cc807b0fd9c352d38941fa876d30675ef
abdoheddaya <heddayaabderrahmane@gmail.com>
2026-04-27 10:29:44 +0100

    Init commit

---
+++ ebanking-backend/src/test/java/hattabi/youness/ebanking_backend/EbankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hattabi.youness.ebanking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
