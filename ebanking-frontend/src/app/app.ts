4c7cbbd712ddd9380b6e30ef1e4f6bb6894ed73f
abdoheddaya <heddayaabderrahmane@gmail.com>
2026-05-01 21:01:08 +0100

    navbar added.

--- ebanking-frontend/src/app/app.ts
+++ ebanking-frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Navbar],
+  styleUrl: './app.css',
+  template: `<app-navbar />
+    <div class="container mt-4">
+      <router-outlet />
+    </div> `,
 })
-export class App {
-  protected readonly title = signal('ebanking-frontend');
-}
+export class App {}
---
+++ ebanking-frontend/src/app/components/navbar/navbar.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [CommonModule, RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {
+  authService = inject(AuthService);
+  router = inject(Router);
+
+  logout() {
+    this.authService.logout();
+  }
+}
