1192f7ffd4eda65b1e6c8e263ea9b6075153d405
abdoheddaya <heddayaabderrahmane@gmail.com>
2026-05-01 19:47:08 +0100

    App setup is done.

--- ebanking-frontend/src/app/app.routes.ts
+++ ebanking-frontend/src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { adminGuard, authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login').then((m) => m.Login),
+  },
+  {
+    path: 'customers',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () => import('./components/customers/customers').then((m) => m.Customers),
+  },
+  {
+    path: 'customers/new',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./components/customer-form/customer-form').then((m) => m.CustomerForm),
+  },
+  {
+    path: 'customers/edit/:id',
+    canActivate: [authGuard, adminGuard],
+    loadComponent: () =>
+      import('./components/customer-form/customer-form').then((m) => m.CustomerForm),
+  },
+  {
+    path: 'accounts',
+    canActivate: [authGuard],
+    loadComponent: () => import('./components/accounts/accounts').then((m) => m.Accounts),
+  },
+  { path: '', redirectTo: 'customers', pathMatch: 'full' },
+  { path: '**', redirectTo: 'customers' },
+];
