29f3368eca2fae8ec5ffc06c8c8ddbe013d9b9a3
abdoheddaya <heddayaabderrahmane@gmail.com>
2026-05-02 21:05:03 +0100

    initialized chatbot spring project.

---
+++ ebanking-chatbot/src/main/java/hattabi/youness/ebanking_chatbot/EbankingChatbotApplication.java
@@ -0,0 +1,13 @@
+package hattabi.youness.ebanking_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankingChatbotApplication.class, args);
+	}
+
+}
---
+++ ebanking-chatbot/src/test/java/hattabi/youness/ebanking_chatbot/EbankingChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package hattabi.youness.ebanking_chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankingChatbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
